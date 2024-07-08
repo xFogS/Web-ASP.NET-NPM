27ff26ec81b92f4b50844f7efd33bc5788c8b0b3
xFogS <romanprotsenko4@gmail.com>
2024-07-08 16:28:02 +0300

    update. Append Class - HomeAnimals,WildAnimals

---
+++ Web ASP.NET/Controllers/HomeAnimalsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_ASP.NET.Controllers
+{
+    public class HomeAnimalsController : Controller
+    {
+        public string Cat()
+        {
+            return "<h1>Cat</h1>";
+        }
+        public string Dog()
+        {
+            return "<h1>Dog</h1>";
+        }
+        public string Bird()
+        {
+            return "<h1>Bird</h1>";
+        }
+    }
+}
---
+++ Web ASP.NET/Controllers/WildAnimalsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_ASP.NET.Controllers
+{
+    public class WildAnimalsController : Controller
+    {
+        public string Bear()
+        {
+            return "<h1>Bear</h1>";
+        }
+        public string Elephant()
+        {
+            return "<h1>Elephant</h1>";
+        }
+        public string Snake()
+        {
+            return "<h1>Snake</h1>";
+        }
+    }
+}
