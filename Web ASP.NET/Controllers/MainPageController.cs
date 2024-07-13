37e75c2f8bf8dedb9b40f0a1dcdc2aaefe355475
FogS <romanprotsenko4@gmail.com>
2024-07-13 15:59:36 +0300

    Controller and View / MainPage

---
+++ Web ASP.NET/Controllers/MainPageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_ASP.NET.Controllers
+{
+    public class MainPageController : Controller
+    {
+        public IActionResult LoadPage()
+        {
+            ViewData["css_style"] = "css/styles.css";
+            ViewData["favicon"] = "assets/favicon.ico";
+            ViewData["js_fontawesome"] = "https://use.fontawesome.com/releases/v6.3.0/js/all.js";
+            ViewData["fonts_Montserrat"] = "https://fonts.googleapis.com/css?family=Montserrat:400,700";
+            ViewData["fonts_Roboto_Slab"] = "https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700";
+            return View();
+        }
+    }
+}
