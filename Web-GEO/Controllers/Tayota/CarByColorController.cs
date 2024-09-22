6cfbbbe1a99bbdc782b15873bb1ea5df923ed5a2
xFogS <romanprotsenko4@gmail.com>
2024-09-22 22:45:24 +0300

    Upd Tayota

---
+++ Web-GEO/Controllers/Tayota/CarByColorController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web_GEO.Data;
+
+namespace Web_GEO.Controllers.Tayota
+{
+    public class CarByColorController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CarByColorController(ApplicationDbContext context) { _context = context; }
+        public async Task<IActionResult> Index(int? modelId = null, int? configurationId = null, int? colorId = null)
+        {
+            if(modelId == null) { modelId = 1; configurationId = 1; colorId = 11; } //11 default white
+
+            ViewBag.models = new SelectList(await _context.TayotaModels.ToListAsync(), "Id", "Name", modelId);
+            if (modelId != null)
+            {
+                var configuration = await _context.ConfigurationModels
+                    .Where(c => c.ModelId == modelId)
+                    .ToListAsync();
+                ViewBag.configurations = new SelectList(configuration, "Id", "Name", configurationId);
+            }
+            
+            if (configurationId != null)
+            {
+                var colors = await _context.ConfigurationColorModels
+                    .Include(c => c.ColorModel)
+                    .Where(c => c.ConfigurationId == configurationId)
+                    .ToListAsync();
+                ViewBag.colors = new SelectList(colors, "Id", "ColorModel.Name", colorId);
+            }
+
+            if(colorId != null)
+            {
+                var image = await _context.ConfigurationColorModels
+                    .Where(c => c.ConfigurationId == configurationId)
+                    .FirstOrDefaultAsync(c => c.ColorId == colorId);
+                if (image != null)
+                {
+                    ViewBag.image = image.CarImageUrl;
+                }
+                else
+                {
+                    ViewBag.image = "/storage/colors/unknown.png";
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Web-GEO/Controllers/Tayota/SearchGeoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_GEO.Data;
+
+namespace Web_GEO.Controllers.Tayota
+{
+    public class SearchGeoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public SearchGeoController(ApplicationDbContext context) { _context = context; }
+        public IActionResult Index()
+        {
+            return View(_context.ColorModels.ToList());
+        }
+        public IActionResult SearchPage() { return View("Search"); }
+        [HttpGet]
+        public async Task<IActionResult> Search(string search = null, int pageNumber = 1, int pageSize = 2)
+        {
+            var model = await _context.ColorModels.AsQueryable()
+                  .Include(m => m.ConfColor)
+                  .Where(m => !String.IsNullOrEmpty(search) ? EF.Functions.Like(m.Name, $"%{search}%") : true)
+                  .OrderBy(m => m.Name)
+                  .Skip((pageNumber - 1) * pageSize)
+                  .Take(pageSize)
+                  .ToListAsync();
+            return View(model);
+        }
+    }
+}
